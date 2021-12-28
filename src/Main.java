96cb77a7fce4608c0a6f6275243278085e728e99
AlexVladimirov05690 <93385788+AlexVladimirov05690@users.noreply.github.com>
2021-12-28 12:14:59 +0300

    Added a class for defining sivols that occur in the text.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        char[] symbols;
-        symbols = new char[]{'a', 'b', 'c', 'd'};
-        for (char symbol : symbols) {
-            SoughtChar soughtChar = new SoughtChar("OriginalText.txt", symbol);
+        ArrayAllChar arrayAllChar = new ArrayAllChar("OriginalText.txt");
+        arrayAllChar.finder();
+        for (int i = 0; i < arrayAllChar.allChar.size(); i++) {
+            SoughtChar soughtChar = new SoughtChar("OriginalText.txt", arrayAllChar.allChar.get(i));
             soughtChar.counter();
         }
     }
